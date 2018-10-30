d48e62a5b2fcfbd8200cbfdd1c1e3e19a923e3b6
Ana Scotti <scottianapaula@gmail.com>
2018-10-30 14:29:38 +0100

    Test some new java features

--- main/src/main/java/com/awesome/api/AwesomeController.java
+++ main/src/main/java/com/awesome/api/AwesomeController.java
@@ -5,11 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 public class AwesomeController {
 
     @RequestMapping("/greeting")
     public Customer greeting(@RequestParam(value = "name", defaultValue = "Miew") String name) {
         return new Customer(1L, "199009091234", name);
+    }
+
+    @RequestMapping("/customers")
+    public List<Customer> customers() {
+        var list = new ArrayList<Customer>();
+        list.add(new Customer(1L, "199009091234", "Miew"));
+        list.add(new Customer(2L, "199009094321", "Woof"));
+        return list;
     }
 }
---
+++ main/src/test/java/com/awesome/api/NewInJavaTest.java
@@ -0,0 +1,40 @@
+package com.awesome.api;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class NewInJavaTest {
+
+    @Test
+    public void testUnmodifieble(){
+        var list = new ArrayList<String>();
+        list.add("Miew");
+        var unmodifiable = Collections.unmodifiableList(list);
+        list.add("Woof");
+        assertTrue(unmodifiable.equals(list));
+    }
+
+    @Test
+    public void testUnmodifiebleUnsupportedOperation(){
+        assertThrows(UnsupportedOperationException.class, () -> {
+            var unmodifiable = Collections.unmodifiableList(Collections.EMPTY_LIST);
+            unmodifiable.set(0, "not allowed");
+        });
+    }
+
+    @Test
+    public void testRealUnmodifieble(){
+        var list = Arrays.asList(1, 2, 3, 4);
+        var unmodifiable = list.stream().collect(Collectors.toUnmodifiableList());
+        list.set(0, 4);
+        assertEquals(Arrays.asList(1, 2, 3, 4), unmodifiable);
+    }
+
+}
