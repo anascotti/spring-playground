d9f9ffe2b00a4b373661c9d3f665e29eea0826e5
Ana Scotti <scottianapaula@gmail.com>
2018-10-30 11:51:46 +0100

    Exclude transitive dependencies
    * When building with Intellij transitive depenendencies are places under the module path causing split packages/ module issues

--- main/src/main/java/module-info.java
+++ main/src/main/java/module-info.java
@@ -1,7 +1,3 @@
-/* This causes split packages issue
- * This only works when building with maven which places transitive dependencies in the classpath.
- * Intellij places them in the module path resulting in 'reads from both' errors.
- * */
 module com.awesome {
     requires spring.boot;
     requires spring.boot.autoconfigure;
