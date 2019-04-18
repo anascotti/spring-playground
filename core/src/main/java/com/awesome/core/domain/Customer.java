e2245d21acfc1064251dcd9bcb6de3ce2a273b4a
Ana Scotti <ana.scotti@bambora.com>
2019-04-18 12:21:31 +0200

    Try some java 12 switch expressions

--- core/src/main/java/com/awesome/core/domain/Customer.java
+++ core/src/main/java/com/awesome/core/domain/Customer.java
@@ -4,9 +4,14 @@ package com.awesome.core.domain;
 public class Customer {
 
     protected Customer(Long id, String ssn, String name) {
-     this.id = id;
-     this.ssn = ssn;
-     this.name = name;
+     this(id, ssn, name, Category.X);
+    }
+
+    protected Customer(Long id, String ssn, String name, Category category) {
+        this.id = id;
+        this.ssn = ssn;
+        this.name = name;
+        this.category = category;
     }
 
     public final Long id;
@@ -15,10 +20,25 @@ public class Customer {
 
     public final String name;
 
+    public final Category category;
+
 
     @Override
     public String toString() {
         return "Customer{" + "id=" + id + ", ssn='" + ssn + '\'' + ", name='" + name + '\'' + '}';
+    }
+
+    public enum Category {
+        A, B, C, D, E, X;
+    }
+
+    public int discountRate(){
+        return switch (category) {
+            case X, E -> 0;
+            case D, C -> 5;
+            case B -> 10;
+            case A -> 15;
+        };
     }
 
 }
